b12c4b459e119cc9b31298ea03747bf72e57c623
Trong <protrongkhh@gmail.com>
2026-03-23 15:47:42 +0700

    user, bidder và entity

--- src/main/java/Model/User/Bidder.java
+++ src/main/java/Model/User/Bidder.java
@@ -1,4 +1,14 @@
 package Model.User;
+// Kế thừa phân cấp rõ ràng
+public class Bidder extends User {
 
-public class Bidder {
+    public Bidder(int id, String username, String password, String email) {
+        super(id, username, password, email);
+    }
+
+    // Ghi đè phương thức để thể hiện tính đa hình (Polymorphism) [cite: 121]
+    @Override
+    public void displayRole() {
+        System.out.println("Vai trò: Người tham gia đấu giá (Bidder)");
+    }
 }
