13a458df90c842aa2438bd2860a9c72b2b616018
thai2007dz <thai2007dz@gmail.com>
2026-03-28 23:46:23 +0700

    init frontend structure and main window

---
+++ src/main/java/frontend/MainUI.java
@@ -0,0 +1,17 @@
+package frontend;
+
+import javax.swing.JFrame;
+
+public class MainUI {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Auction System");
+
+        frame.setSize(800,600);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
